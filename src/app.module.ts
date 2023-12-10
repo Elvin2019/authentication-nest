5a7d4ffd69a80edccf401d25a08275552c397ec4
elvin2019 <elvinahmadov0@gmail.com>
2023-12-11 00:41:32 +0400

    Latest updates for docker and configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/auth.guard';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './health/health.controller';
@@ -18,45 +18,61 @@ import { redisStore } from 'cache-manager-redis-yet';
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
-    MongooseModule.forRoot('mongodb://root:example@localhost:27017/', {
-      dbName: 'auth',
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          uri: configService.get('DATABASE_URI'),
+          dbName: configService.get('DATABASE_NAME'),
+        };
+      },
+
+      inject: [ConfigService],
     }),
-    LoggerModule.forRoot({
-      pinoHttp: {
-        customProps: (req, res) => ({
-          context: 'HTTP',
-        }),
-        transport: {
-          targets: [
-            {
-              target: 'pino-pretty',
-              options: {
-                singleLine: true,
-                colorize: true,
+    LoggerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        pinoHttp: {
+          customProps: (req, res) => ({
+            context: 'HTTP',
+          }),
+          transport: {
+            targets: [
+              {
+                target: 'pino-pretty',
+                options: {
+                  singleLine: true,
+                  colorize: true,
+                },
               },
-            },
-            {
-              target: 'pino-mongodb',
-              level: 'info',
-              options: {
-                uri: 'mongodb://root:example@localhost:27017/',
-                database: 'auth',
-                collection: 'logs',
+              {
+                target: 'pino-mongodb',
+                level: 'info',
+                options: {
+                  uri: configService.get('DATABASE_URI'),
+                  database: configService.get('DATABASE_NAME'),
+                  collection: 'logs',
+                },
               },
-            },
-          ],
+            ],
+          },
         },
-      },
+      }),
+      inject: [ConfigService],
     }),
-    CacheModule.register<RedisClientOptions>({
-      store: redisStore,
-      // username: process.env.REDIS_USER,
-      password: process.env.REDIS_PASSWORD,
+    CacheModule.registerAsync<RedisClientOptions>({
+      imports: [ConfigModule],
       isGlobal: true,
-      socket: {
-        host: process.env.REDIS_HOST ?? 'localhost',
-        port: parseInt(process.env.REDIS_PORT ?? '6379'),
-      },
+      useFactory: async (configService: ConfigService) => ({
+        store: redisStore,
+        password: configService.get('REDIS_PASSWORD'),
+        isGlobal: true,
+        socket: {
+          host: configService.get('REDIS_HOST'),
+          port: configService.get('REDIS_PORT'),
+        }
+      }),
+      inject: [ConfigService],
     }),
     TerminusModule,
     HttpModule,
