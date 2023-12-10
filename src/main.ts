19e58b2c699b2b2abc9998b0f26dd8889bc02ec6
elvin2019 <elvinahmadov0@gmail.com>
2023-12-10 19:43:01 +0400

    middleware exceptions filter

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
+import { AllExceptionsFilter } from './middleware/all-exceptions-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {bufferLogs: true});
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger));
   app.useGlobalInterceptors(new LoggerErrorInterceptor());
   app.enableCors()
-
   const options = new DocumentBuilder()
     .setTitle('API')
     .setDescription('Authentication system')
